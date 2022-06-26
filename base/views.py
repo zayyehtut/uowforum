78d0e69eeb85c7bd1d8e41d400bd0ce5b5a1a41b
zayyehtut <zayyehtut.zy@gmail.com>
2022-06-27 03:48:30 +0630

    Added login_logout

--- base/views.py
+++ base/views.py
@@ -1,17 +1,43 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.db.models import Q
 from django.shortcuts import redirect, render
 
 from .forms import RoomForm
 from .models import Room, Topic
 
-""""
-rooms = [
-    {"id": 1, "name": "Let's Learn Python"},
-    {"id": 2, "name": "Design with me"},
-    {"id": 3, "name": "Frontend Developer"},
-]
-"""
 # Create your views here.
+
+
+def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User not found")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Wrong Credentials")
+
+    context = {}
+    return render(request, "base/login_register.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
+
+
 def home(request):
 
     q = request.GET.get("q") if request.GET.get("q") != None else ""
